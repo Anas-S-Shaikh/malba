f3c7af76f1b3f700e858438ad35b425c5666e3c9
Anas-S-Shaikh <anasshaikh3343@gmail.com>
2021-12-13 23:40:16 +0530

    Routes setup for specific service detials page and edit service

--- src/RoutesProvider.js
+++ src/RoutesProvider.js
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import ProductDetails from "./Pages/Product/ProductDetails";
 import AddProduct from "./Pages/Products/AddProduct";
 import AllProducts from "./Pages/Products/AllProducts";
+import AddService from "./Pages/Services/AddService";
+import ServiceDetails from "./Pages/Services/ServiceDetails";
 import { routes } from "./routes";
 const NotFound = () => {
   return <h1>404</h1>;
@@ -21,10 +23,10 @@ const RoutesProvider = () => {
         );
       })}
       <Route path="/product-details/:id" element={<ProductDetails />} />
-      <Route path="/service-details/:id" element={<ProductDetails />} />
+      <Route path="/service-details/:id" element={<ServiceDetails />} />
       <Route path="/products/:id" element={<AllProducts />} />
       <Route path="/product/edit/:id" element={<AddProduct />} />
-      <Route path="/service/edit/:id" element={<AddProduct />} />
+      <Route path="/service/edit/:id" element={<AddService />} />
     </Routes>
   );
 };
